58efd7792ae0776cc5a16834e685c36a3e72e26e
Ethan600 <77286763+Ethan600@users.noreply.github.com>
2022-01-05 19:43:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
-function preload(){}
+function preload(){
+
+}
 
 function setup(){
     canvas = createCanvas(400, 400);
-    canvas.center();
-
-
+    canvas.position(430, 160)
     video = createCapture(VIDEO);
     video.hide();
-
-    tint_color = "";
-}
-
-function Tinter(){
-    tint_color = document.getElementById("color_name").value;
-}
-
-function take_snapshot(){
-    save("Tint color " + tint_color + ".png");
 }
 
 function draw(){
-    image(video, 0 , 0, 400, 400);
-    tint(tint_color);
+    image(video, 50, 50, 300, 300);
+
+    fill(0, 255, 0);
+    stroke(0, 255, 0);
+
+    rect(50, 50, 20, 300);
+    rect(350, 50, 20, 300);
+    rect(50, 50, 300, 20);
+    rect(50, 350, 300, 20);
+
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+
+    circle(60, 60, 70 );
+    circle(360, 60, 70);
+    circle(60, 360, 70);
+    circle(360, 360, 70);   
+}
+
+function takeSnapshot(){
+    save("Pic_with_frame.jpg")
 }
